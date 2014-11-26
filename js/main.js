9aa3487513aaad398b96dfb340819471c775bc8e
andrei <laknokal@gmail.com>
2014-11-26 19:51:36 +0200

    first section on full screen, animate to top link, header markup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 jQuery(function ($) {
 
+    //region ===== Variables =====
+    var windowHeight = $(window).height();
+    //endregion
 
+    //region ========== Scroll to the top of the page ==========
+    $(".upward").on("click", function (e) {
+        e.preventDefault();
+
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
+    });
+    //endregion
+
+    //region ===== Calculate welcome block height =====
+    $("#welcome").height(windowHeight);
+    //endregion
 
 });
